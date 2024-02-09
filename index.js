e754f1943b8715f8aaeee134b65dd491fe4fd26f
yilong <yiiloongg@gmail.com>
2024-02-09 21:55:00 +0800

    add uptime

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const http = require("http");
+const os = require("os");
 
 const server = http.createServer((req, res) => {
     res.writeHead(200, { "Content-Type": "text/plain" });
+    res.write("This os running uptime is: " + os.uptime() + "\n");
     res.end("Hello, World!");
 });
 
